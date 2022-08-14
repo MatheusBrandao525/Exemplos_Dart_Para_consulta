bdc4446e3ce7a6d3d6ebe41d711285047a7e614b
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-08-14 16:53:15 -0400

    Mapas

---
+++ aula09.dart
@@ -0,0 +1,28 @@
+void main() async{
+
+  // Mapas
+  
+  // Iniciando o Map.
+  Map<int, String> mapa = {1: 'Valor'};
+  print(mapa);
+  
+  // Adicionando novos valores ao Map.
+  mapa.putIfAbsent(2,()=> 'valor02');
+  mapa.putIfAbsent(3,()=> 'valor03');
+  mapa.putIfAbsent(4,()=> 'valor04');
+  print(mapa);
+  
+  
+  // Removendo valores do Map
+  mapa.remove(2);
+  print(mapa);
+  
+  // Fazendo ForEach para exibir todos os valores do Map.
+  mapa.forEach((chave, valor)=> print('a chave eh: $chave, o valor eh: $valor'));
+  
+  // Fazendo ForEach para exibir todas as chaves do Map.
+  mapa.keys.forEach(print);
+  
+  // Fazendo ForEach para exibir todos os valores do Map.
+  mapa.values.forEach(print);
+}
