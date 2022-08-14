542a907e189b51552af745c5847a96b9bd42bcd7
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-08-14 15:14:04 -0400

    Future, Async e Await

---
+++ aula08.dart
@@ -0,0 +1,19 @@
+void main() async{
+
+  // Future, Async e Await
+  String name = 'Matheus';
+  Future<String> cepFuture = getCepByName('BR 429 km90');
+  late String cep;
+  
+  cep = await cepFuture; // await daz com que a execuçao aguarde ate que a variavel tenha recebido o seu valor.
+  
+  print(cep);
+}
+
+
+
+// serviço externo
+Future<String> getCepByName(String name){ // Future diz a aplicação que uma variavel receberá um valor no futuro.
+  // simulando chamada em serviço externo
+  return Future.value('76935000');
+}
