09570ae204d55010b3d5beedfe45d50dd32dc6de
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-08-14 17:18:06 -0400

    Json

---
+++ aula10.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+void main() {
+
+  // Json
+  
+  // Criando Json
+  String json = ''' 
+  {
+    "Usuario" : "mafe123silva@gmail.com",
+    "Senha" : "123456",
+    "Permisions" : [ 
+      "USER", "ADMIN"
+    ]
+  } 
+  ''';
+  
+  print(json);
+  
+  // jsonDecode converte json para Map.
+  Map resultJson = jsonDecode(json);
+  print(resultJson["Permisions"][1]);
+  
+  
+  // usando jsonEncode.
+  Map mapa = {
+    'Usuario': 'matheus',
+    'Password': 123456,
+    'Permisions': ['owner', 'admin']
+  };
+  print(mapa);
+  
+  print('Convertendo objeto para Json');
+  var result = jsonEncode(mapa);
+  print(result);
+}
