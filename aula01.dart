cb33c2e02a2942c51c3f065d741df69b7dc726da
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-08-14 14:30:41 -0400

    AULA_01 introdução a variaveis e concatenação.

---
+++ aula01.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // AULA_01 introdução a variaveis e concatenação.
+
+  String nome = 'Matheus';
+  print('Nome:  $nome');
+
+  int idade = 20;
+  print('Idade: $idade');
+
+  bool ehBrasileiro = true;
+  print('É brasileiro? $ehBrasileiro');
+
+  List<String> habilidades = ['Estudioso', 'Proativo', 'Esperto'];
+
+  print(
+      'Habilidades: ${habilidades[0]} - ${habilidades[1]} - ${habilidades[2]}');
+}
