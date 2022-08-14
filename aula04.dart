3005e7cd5d5def7abe5adef70b4236ba30691b1d
Matheus Felipe Brandão da Silva <77033014+MatheusBrandao525@users.noreply.github.com>
2022-08-14 14:33:11 -0400

    AULA_04 Estruturas de repetição.

---
+++ aula04.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // AULA_04 Estruturas de repetição.
+
+  // tabuada do 5
+  print('Tabuada do 5');
+  for (int i = 1; i <= 10; i++) {
+    print(5 * i);
+  }
+
+  // loop
+  print('Loop de repetiçaõ');
+  int contador = 10;
+  while (contador != 1) {
+    contador = contador - 1;
+    print(contador);
+  }
+}
